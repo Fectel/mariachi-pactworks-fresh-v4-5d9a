302418d4d4587a5ee00df7bb32b01c6cb21073f3
Fectel <HectorGamador@gmail.com>
2026-09-08 17:53:41 -0700

    Delete mariachi-pactworks-v4_6A4_NEW_SIGNING_VERSION_RAILWAY_FLAT directory

--- mariachi-pactworks-v4_6A4_NEW_SIGNING_VERSION_RAILWAY_FLAT/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-rm -rf /srv
-mkdir -p /srv
-cp -R /srv-template/. /srv/
-for file in $(find /srv -type f -name '*.html'); do tmp="${file}.tmp"; envsubst '$GOOGLE_MAPS_BROWSER_KEY $PUBLIC_REQUEST_API_URL' < "$file" > "$tmp"; mv "$tmp" "$file"; done
-exec caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
